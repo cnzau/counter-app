2cc8b8b57e514fcd71691147684257bb9b0f20a9
Clement Nzau <clement.nzau@gmail.com>
2018-10-04 00:45:38 +0300

    Render lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,19 +3,25 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: "https://picsum.photos/200"
+    tags: ["tag1", "tag2", "tag3"]
   };
 
   render() {
     return (
       <div>
-        {/* Get badge classes from a metthod in this class */}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {/* Map each element in tags array into a react element 
+              Include a unique key for each iterator in a particular list for id*/}
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
   }
-  //Extracted and Refactored from variables in return method to create new method
+
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
@@ -23,9 +29,7 @@ class Counter extends Component {
   }
 
   formatCount() {
-    // Object Destructuring (this.state.count --> { count })
     const { count } = this.state;
-    // Embedding js/jsx expressions
     return count === 0 ? "Zero" : count;
   }
 }
