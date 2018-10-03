9afaf89cad73e2e12ef09f1e443a2b560c4d1c55
Clement Nzau <clement.nzau@gmail.com>
2018-10-04 01:06:20 +0300

    Conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,21 +3,29 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    tags: []
   };
+
+  // Conditional rendering content
+  renderTags() {
+    // Display message if there is no tag or Null if nothing is to be displayed
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    // Render tags if there is atleast one of them
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
 
   render() {
     return (
       <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {/* Map each element in tags array into a react element 
-              Include a unique key for each iterator in a particular list for id*/}
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        {/* Conditional rendering with logical && and a non boolean operand */}
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
       </div>
     );
   }
