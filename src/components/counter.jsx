e4aadc6e2b38beafa8d29510f3b9b33381b94af5
Clement Nzau <clement.nzau@gmail.com>
2018-10-04 00:09:11 +0300

    Encapsulate dynamic classes statements into a new method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,34 +5,28 @@ class Counter extends Component {
     count: 0,
     imageUrl: "https://picsum.photos/200"
   };
-  styles = {
-    fontSize: 10,
-    fontWeight: "bold"
-  };
 
   render() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
     return (
-      <React.Fragment>
-        {/* Setting attributes using state
-            Inline styling */}
-        <img style={{ borderRadius: 5 }} src={this.state.imageUrl} alt="" />
-        {/* Style using state Object
-            Embedding js/jsx expressions
-            Object destructuring for this.state.classes */}
-        <span style={this.styles} className={classes}>
-          {this.formatCount()}
-        </span>
+      <div>
+        {/* Get badge classes from a metthod in this class */}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
-      </React.Fragment>
+      </div>
     );
   }
+  //Extracted and Refactored from variables in return method to create new method
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     // Object Destructuring (this.state.count --> { count })
     const { count } = this.state;
     // Embedding js/jsx expressions
-    return count === 0 ? <h6>Zero</h6> : count;
+    return count === 0 ? "Zero" : count;
   }
 }
 
