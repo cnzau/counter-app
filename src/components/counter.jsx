44ecd77f4ad272be078cae497648aea2ce3d9182
Clement Nzau <clement.nzau@gmail.com>
2018-10-03 23:45:47 +0300

    Dynamically set attributes Use Inline and state object styling Embed jsx expressions Use object destructuring

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,38 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/200"
+  };
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold"
+  };
+
   render() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
     return (
       <React.Fragment>
-        <h1>Hello World</h1>
-        <button>Increment</button>
+        {/* Setting attributes using state
+            Inline styling */}
+        <img style={{ borderRadius: 5 }} src={this.state.imageUrl} alt="" />
+        {/* Style using state Object
+            Embedding js/jsx expressions
+            Object destructuring for this.state.classes */}
+        <span style={this.styles} className={classes}>
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
+  }
+  formatCount() {
+    // Object Destructuring (this.state.count --> { count })
+    const { count } = this.state;
+    // Embedding js/jsx expressions
+    return count === 0 ? <h6>Zero</h6> : count;
   }
 }
 
