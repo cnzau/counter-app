1bd5f52b5d3853b32dfd72cc9dca2a344e686dcf
Clement Nzau <clement.nzau@gmail.com>
2018-10-10 01:14:42 +0300

    Clean code by using object destructuring args

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,22 +5,22 @@ class Counters extends Component {
   // Controlled componet bt the app component
   // No state, receives data and methods to modify the data by props
   render() {
+    // Use object destructuring to pick the properties of the props object you interested in
+    const { onReset, counters, onDelete, onIncrement } = this.props;
+
     return (
       <div>
-        <button
-          onClick={this.props.onReset}
-          className="btn btn-primary btn-sm m-2"
-        >
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
           Reset
         </button>
         {/* Render each counter using map method */}
-        {this.props.counters.map(counter => (
+        {counters.map(counter => (
           // Event is not handled here
           // Raise an event with onDel... which is then bubbled up to the parent with this.props.onDel...]
           <Counter
             key={counter.id}
-            onDelete={this.props.onDelete}
-            onIncrement={this.props.onIncrement}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
             counter={counter}
           />
         ))}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,14 +3,16 @@ import React, { Component } from "react";
 // Stateless Functional Component (sfc-->)
 // 'this' only works in class components for SFC you need to add props as parameter
 // has one render method, no event handlers, no helper methods to cacilate values, no state, all data via props
-const NavBar = props => {
+
+//Use object destructuring instead of props
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Navbar{" "}
         <span className="badge badge-pill badge-secondary">
           {/* Remove 'this' since it only works in class components */}
-          {props.totalCounters}
+          {totalCounters}
         </span>
       </a>
     </nav>
