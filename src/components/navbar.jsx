bc096193d55dc3579f05e091cb5ea31f0daebcc9
Clement Nzau <clement.nzau@gmail.com>
2018-10-09 20:55:05 +0300

    Convert NavBar class component to stateless functional component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar{" "}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+// Stateless Functional Component (sfc-->)
+// 'this' only works in class components for SFC you need to add props as parameter
+// has one render method, no event handlers, no helper methods to cacilate values, no state, all data via props
+const NavBar = props => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {/* Remove 'this' since it only works in class components */}
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
