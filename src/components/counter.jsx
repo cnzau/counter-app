c1aeac752b1c5b7607ba8b076c008e655b9d6600
Clement Nzau <clement.nzau@gmail.com>
2018-10-12 01:03:01 +0300

    Space out counter's elements Apply disable attr for (-) btn dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -31,31 +31,40 @@ class Counter extends Component {
     console.log(this.props);
     // Logs {onDelete: f, value: *, id: *}
     return (
-      <div>
+      <div className="row">
+        <div className="col-1">
+          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        </div>
         {/* Render passed attributes instead of children by using props in elements  */}
-        <h4>Counter #{this.props.counter.id}</h4>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button
-          // pass name of our event onIncrement
-          // Pass reference to the counter object making implementation of event handler simple
-          onClick={() => this.props.onIncrement(this.props.counter)}
-          className="btn btn-secondary btn-sm"
-        >
-          +
-        </button>
-        <button
-          onClick={() => this.props.onDecrement(this.props.counter)}
-          className="btn btn-secondary btn-sm"
-        >
-          -
-        </button>
-        <button
-          // pass id of this counter
-          onClick={() => this.props.onDelete(this.props.counter.id)}
-          className="btn btn-danger btn-sm m-2"
-        >
-          Delete
-        </button>
+        {/* <h4>Counter #{this.props.counter.id}</h4>
+        // <span className={this.getBadgeClasses()}>{this.formatCount()}</span> */}
+
+        <div className="col">
+          <button
+            // pass name of our event onIncrement
+            // Pass reference to the counter object making implementation of event handler simple
+            onClick={() => this.props.onIncrement(this.props.counter)}
+            className="btn btn-secondary btn-sm"
+          >
+            +
+          </button>
+
+          <button
+            onClick={() => this.props.onDecrement(this.props.counter)}
+            className="btn btn-secondary btn-sm m-2"
+            disabled={this.props.counter.value === 0 ? "disabled" : ""}
+          >
+            -
+          </button>
+
+          <button
+            // pass id of this counter
+            onClick={() => this.props.onDelete(this.props.counter.id)}
+            className="btn btn-danger btn-sm"
+          >
+            Delete
+          </button>
+        </div>
       </div>
     );
   }
